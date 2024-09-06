b946295ba9dd4d3893eea1560deb1b755bf1d7a3
rytst <r.saito0111@gmail.com>
2024-09-07 01:16:44 +0900

    add assignment_operator.cpp

---
+++ basics_of_cpp/operators/assignment_operator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    a = 10;
+    b = 4;
+    a = b;
+    b = 7;
+
+    cout << "a: ";
+    cout << a;
+    cout << " b: ";
+    cout << b << endl;
+
+}
