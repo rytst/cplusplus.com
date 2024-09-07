c999e0131607dd4972d2638a426557e2a7bd5cb1
rytst <r.saito0111@gmail.com>
2024-09-08 00:15:39 +0900

    jump statement

---
+++ program_structure/statements_and_flow_control/the_break_statement.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+    for (int n = 10; n > 0; n--)
+    {
+        cout << n << ", ";
+
+	if (n == 3)
+	{
+            cout << "countdown aborted!" << endl;
+            break;
+	}
+    }
+
+    return 0;
+}
