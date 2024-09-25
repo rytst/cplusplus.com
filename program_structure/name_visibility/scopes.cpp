e149f23e278be38e18b753f1452165015423160a
rytst <rytst@pop-os.lan>
2024-09-26 00:22:57 +0900

    complete scopes

--- program_structure/name_visibility/scopes.cpp
+++ program_structure/name_visibility/scopes.cpp
@@ -6,8 +6,11 @@ int foo; // global variable
 
 int some_function()
 {
-    int bar; // local variable
+    int bar;     // local variable
     bar = 0;
+
+ // double bar;
+ // bar = 0.0;   // wrong: name already used in this scope
 }
 
 int other_function()
@@ -17,7 +20,25 @@ int other_function()
 }
 
 
+// Variables declared in declarations that introduce a block,
+// such as function parameters and variables declared in loops
+// and conditions are local to the block they introduce.
 int main()
 {
+    int x = 10;
+    int y = 20; 
+    {
+        int x;  // ok: inner scope. x is hidden.
+        x = 50; // sets value to inner x
+        y = 50; // sets value to (outer) y
+
+        cout << "inner block" << endl;
+        cout << "x: " << x << endl;
+        cout << "y: " << y << endl;
+    }
+    cout << "outer block" << endl;
+    cout << "x: " << x << endl;
+    cout << "y: " << y << endl;
+
     return 0;
 }
