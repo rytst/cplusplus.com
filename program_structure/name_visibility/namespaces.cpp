8cda99ab14297e749105c5071e495ef2a85f6ff0
rytst <rytst@pop-os.lan>
2024-09-26 00:54:18 +0900

    complete namespaces

--- program_structure/name_visibility/namespaces.cpp
+++ program_structure/name_visibility/namespaces.cpp
@@ -2,8 +2,30 @@
 
 using namespace std;
 
+namespace foo
+{
+    int value()
+    {
+        return 5;
+    }
+}
+
+namespace bar
+{
+    const double pi = 3.1416;
+    double value()
+    {
+        return 2 * pi;
+    }
+}
+
 
 int main()
 {
+    cout << foo::value() << endl;
+    cout << bar::value   << endl;
+    cout << bar::pi      << endl;
+
     return 0;
 }
+
